f8d91cd8f760fc3645e63b37d7189e46c98e9af0
kothari-Ak <kotharianjali13@gmail.com>
2023-01-13 20:05:27 +0530

    palin nd anagram

---
+++ IsPalindromeOrNot.js
@@ -0,0 +1,12 @@
+const isPalindromeOrNot = (str) => {
+    if(!str.length) return true;
+   let left  = 0, right = str.length - 1;
+   while(left<right){
+    if(str[left]!==str[right])return false;
+    left++;
+    right--;
+   }
+   return true;
+}
+
+console.log(isPalindromeOrNot('level'))
